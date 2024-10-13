29ab3d07b8ec8b5f7111a8a9ca953f0d1d4d32eb
AlexxGutt <alex.gutt89@gmail.com>
2024-10-14 00:44:35 +0300

    done 2 task

---
+++ hw-6.js
@@ -0,0 +1,20 @@
+// Task - 1
+let password = String(`пароль`);
+let userPassword = String(prompt(`Введите пароль`));
+
+if (password === userPassword) {
+console.log(`Пароль введен верно`);
+} else {
+console.log(`Пароль введен неправильно`);     
+}
+
+// Task - 2
+let c = 2;
+if (c >= 0 && c <= 10) {
+    console.log(`Верно`);
+} else {
+    console.log(`Неверно`)
+}
+
+// Task - 3
+
