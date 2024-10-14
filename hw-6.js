ff415bc3cd3f570cb816cd438c4e2daaa4bdad88
AlexxGutt <alex.gutt89@gmail.com>
2024-10-14 15:20:24 +0300

    Сделал основное задание.

--- hw-6.js
+++ hw-6.js
@@ -17,4 +17,72 @@ if (c >= 0 && c <= 10) {
 }
 
 // Task - 3
+let d = 99
+let e = 100
+
+if (d >= 100 || e >= 100) {
+    console.log(`Верно`);
+} else {
+    console.log(`Неверно`);
+}
+
+// Task - 4
+let a = `2`;
+let b = `3`;
+
+console.log(Number(a) + Number(b));
+
+// Task - 5
+
+let monthNumber = Number(prompt(`Введите число месяца`));
+
+switch(monthNumber) {
+    case 1:
+        console.log(`Зимушка зима`);
+        break;
+    case 2:
+        console.log(`Зимушка зима`);
+        break;
+    case 3:
+        console.log(`Весна`);
+        break;
+    case 4:
+        console.log(`Весна`);
+        break;
+    case 5:
+        console.log(`Весна`);
+        break;
+    case 6:
+        console.log(`Лето`);
+        break;     
+    case 7:
+        console.log(`Лето`);
+        break;
+    case 8:
+        console.log(`Лето`);
+        break;
+    case 9:
+        console.log(`Осень`);
+        break;  
+    case 10:
+        console.log(`Осень`);
+        break;
+    case 11:
+        console.log(`Осень`);
+        break;
+    case 12:
+        console.log(`Зимушка зима`);
+        break;
+    default:
+        console.log(`Число не соответствует месяцу года...`);
+}
+
+// Task - 6
+let number = Number(prompt(`Пожалуйста, введите любое число`));
+if (number % 2 === 0) {
+    console.log(`Четное`);
+} else {
+    console.log(`Не четное`);
+}
+
 
