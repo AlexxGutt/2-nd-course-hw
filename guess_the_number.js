a21066447c88dfa76b048c2941a278e87c844e84
AlexxGutt <alex.gutt89@gmail.com>
2024-10-24 19:33:05 +0300

    попытка 5

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,15 +1,19 @@
+const randomNum = Math.floor(Math.random() * 100) + 1;
+console.log(randomNum);
+
 function guessTheNumber() {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
-    console.log(randomNum);
     let num = Number(prompt(`Угадайте число от 1 до 100`));
 
-    if (isNaN(num) || num > 100 || num < 1) {
-        alert(`Пожалуйста, введите число от 1 до 100`);
+    if (num === randomNum) {
+        alert(`Поздравляю! Вы угадали!`);
+        } else if (isNaN(num) || num > 100 || num < 1) {
+            alert(`Введите число от 1 до 100!`);
+            alert(guessTheNumber());
         } else if (num > randomNum) {
-            alert(`Загаданное число меньше ${num}`);
+            alert(`Введите число меньше ${num}`);
+            alert(guessTheNumber());
         } else if (num < randomNum) {
-            alert(`Загаданное число больше ${num}`);
-        } else {
-            alert(`Поздравляю! Вы угадали!`);
-    }
+            alert(`Введите число больше ${num}`);
+            alert(guessTheNumber());
+    } 
 };
