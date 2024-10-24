14523fa56b11eda375dfbc764c0915f32662e805
AlexxGutt <alex.gutt89@gmail.com>
2024-10-24 12:45:10 +0300

    2 - done

--- hw-8.js
+++ hw-8.js
@@ -1,5 +1,18 @@
 // Задание - 1
-let min = (a, b) => { return (a - b) < 0 ? a : b; };
-console.log(min(21, 0));
+let minSearch = (a, b) => { return (a - b) < 0 ? a : b; };
+console.log(minSearch(4, 8));
 
 // Задание - 2
+let c = Number(prompt(`Введите число`));
+function evenNumber(c) {
+            if (c % 2 === 0) {
+            return (`число четное`);
+        }
+        else {
+            return (`число нечетное`);
+        }
+};
+console.log(evenNumber(c));
+
+// Задание - 3
+
