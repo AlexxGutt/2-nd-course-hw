890e88a39eb43fb035eb213d3f862c925d9b7bb7
AlexxGutt <alex.gutt89@gmail.com>
2024-10-24 09:20:43 +0300

    1 - done

--- hw-8.js
+++ hw-8.js
@@ -1,3 +1,5 @@
 // Задание - 1
-let min = (a, b) => { return a < b ? a : b; };
-console.log(min(4, 8));
+let min = (a, b) => { return (a - b) < 0 ? a : b; };
+console.log(min(21, 0));
+
+// Задание - 2
