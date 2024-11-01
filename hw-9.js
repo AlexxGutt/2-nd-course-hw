d503e665aeea67e692e52a937b8227fe158ba2cf
AlexxGutt <alex.gutt89@gmail.com>
2024-11-01 14:24:45 +0300

    Home wor - 9 (start)

---
+++ hw-9.js
@@ -0,0 +1,22 @@
+// Task - 1
+
+const newArr = [1, 5, 4, 10, 0, 3];
+    for (let i = 0; i < newArr.length; i++) {
+        console.log(newArr[i]);
+        if (newArr[i] === 10) {
+            break;
+    }
+};
+
+// Task - 2 
+
+const twoArr = [1, 5, 4, 10, 0, 3]
+twoArr.forEach((el, index) => {
+    if (el === 4) {
+        console.log(index);
+    }
+});
+
+// Task - 3
+
+
