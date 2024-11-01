6acddd779bd14466e5aeb517f7c4286c74316652
AlexxGutt <alex.gutt89@gmail.com>
2024-11-01 15:53:30 +0300

    Task - (Done)

--- hw-9.js
+++ hw-9.js
@@ -34,3 +34,30 @@ for (let a = 0; a < 3; a++) {
     oneArr.push(twoArr);
 };
 console.log(oneArr);
+
+// Task - 5
+
+const fourthArr = [1, 1, 1];
+fourthArr.push(2, 2, 2);
+console.log(fourthArr);
+
+// Task - 6
+
+const fifthArr = [9, 8, 7, 'a', 6, 5];
+fifthArr.sort();
+fifthArr.pop();
+console.log(fifthArr);
+
+// Task - 7
+
+const sixthArr = [9, 8, 7, 6, 5];
+
+const userNumber = Number(prompt(`Введите число`));
+if (sixthArr.includes(userNumber)) {
+    alert(`Вы угадали!`);
+} else {
+    alert(`Вы не удали!`)
+};
+
+// Task - 8
+
