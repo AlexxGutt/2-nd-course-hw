3823f4e1fb3beed4b89613da57c77f13de5985d1
AlexxGutt <alex.gutt89@gmail.com>
2024-11-03 12:27:07 +0300

    Доделал  задания с JS

--- hw-9.js
+++ hw-9.js
@@ -85,3 +85,60 @@ for (let c = 0; c < eighthArr.length; c++) {
 };
 
 // Task - 11
+
+const ninthArr = [5, 6, 9];
+
+function squareOfNumbers () {
+    return ninthArr.map(newNinthArr => newNinthArr ** 2) ;
+}
+
+console.log(squareOfNumbers(ninthArr));
+
+// Task - 12
+
+function wordLength (tenthArr) {
+    return tenthArr.map(newTenthArr => newTenthArr.length);
+}
+
+console.log(wordLength([`Это`, `Весьма`, `Непростая`, `Тема`]));
+
+// Task - 13
+
+function negativeNumbers(eleventhArr) {
+    return eleventhArr.filter(h => h < 0);
+}
+
+console.log(negativeNumbers([1, 3, -10, 5, -8, 78, -61]));
+
+// Task - 14
+
+function randomNumber() {
+    return Math.floor(Math.random() * 10);     
+}
+
+const twelfthArr = [];
+for (let j = 0; j < 10; j++) {
+    twelfthArr.push(randomNumber())   
+}
+console.log(twelfthArr);
+
+const thirteenthArr = [];
+for (let k = 0; k < twelfthArr.length; k++) {
+    if (twelfthArr[k] % 2 === 0) {
+        thirteenthArr.push(twelfthArr[k]);
+    }
+}
+console.log(thirteenthArr);
+
+// Task - 15
+
+function randomNewNumber() {
+    return Math.floor(Math.random() * 10);     
+}
+
+const fourteenthArr = [];
+for (let l = 0; l < 6; l++) {
+    fourteenthArr.push(randomNewNumber())   
+}
+console.log(fourteenthArr);
+console.log(fourteenthArr.reduce((z, x) => z + x) / fourteenthArr.length);
