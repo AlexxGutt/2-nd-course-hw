aa729c9f86fdfd462a0e4ce1a67ba9153ddda63a
AlexxGutt <alex.gutt89@gmail.com>
2024-11-11 22:30:27 +0300

    corrected task - 6

--- hw-10.js
+++ hw-10.js
@@ -5,6 +5,8 @@ console.log(a.toUpperCase());
 
 // Task - 2
 
+
+
 // Task - 3
 
 let numberRoundUp = 32.58884;
@@ -20,8 +22,11 @@ console.log(Math.min(52, 53, 49, 77, 21, 32));
 
 // Task - 5
 
-const randomNumb = Math.floor(Math.random() * 10) + 1;
-console.log(randomNumb);
+function randomNumb() {
+    return Math.floor(Math.random() * 10) + 1;     
+};
+
+console.log(randomNumb());
 
 // Task - 6
 
@@ -30,12 +35,16 @@ let lengthArray = Math.round(integer / 2);
 let newArr = [];
 
 console.log(integer); //для проверки - целое число
-console.log(lengthArray); //для проверки - длина массива 
+console.log(lengthArray); //для проверки - длина массива
 
-for (let i = 0; i < lengthArray; i++) {
-    newArr.push(Math.floor(Math.random() * integer));
-}
+function wholeNumber(newArr) {
+    
+    for (let i = 0; i < lengthArray; i++) {
+        newArr.push(Math.floor(Math.random() * integer));
+    }
+    return newArr;
+};
 
-console.log(newArr);
+console.log(wholeNumber([]));
 
 // Task - 7
