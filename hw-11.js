23da76ee9bc99679be38ccb164eb3e40d70b2fa8
AlexxGutt <alex.gutt89@gmail.com>
2024-11-18 15:06:37 +0300

    Task - 3  - done

--- hw-11.js
+++ hw-11.js
@@ -10,4 +10,34 @@ const people = [
  console.log(people.sort((a, b) => a.age - b.age));
 
 //  Task - 2
+    function filter(array, ruleFunction) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+    };
+
+    function isPositive(num) {
+    return num > 0;
+    };
+
+    function isMale(person) {
+    return person.gender === `male`;
+    };
+    
+    console.log(filter([3, -4, 1, 9], isPositive));
+    
+    const peopleTwo = [
+       {name: 'Глеб', gender: 'male'},
+       {name: 'Анна', gender: 'female'},
+       {name: 'Олег', gender: 'male'},
+       {name: 'Оксана', gender: 'female'}
+    ];
+    
+    console.log(filter(peopleTwo, isMale));
+
+// Task - 3
 
